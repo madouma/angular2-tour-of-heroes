f2e826be92f8ccb6fd714a7730e9ac905376f475
Mamadou Diop <madouma@gmail.com>
2016-12-13 10:54:43 -0600

    - Simulating retieving data from a web server using the "inMemoryWebApi" in favor of mock data - deleted mock data

--- app/hero.service.ts
+++ app/hero.service.ts
@@ -1,16 +1,31 @@
 import { Injectable } from '@angular/core';
+import { Http } from '@angular/http';
+
+import 'rxjs/add/operator/toPromise';
 
 import { Hero } from './hero';
 import { HEROES } from './mock-heroes';
 
 @Injectable()
 export class HeroService {
+    private heroesUrl: string = 'api/heroes';
+
+    constructor(private _http: Http) { }
+
     getHeroes(): Promise<Hero[]> {
-        return Promise.resolve(HEROES);
+        return this._http.get(this.heroesUrl)
+            .toPromise()
+            .then(response => response.json().data as Hero[])
+            .catch(this._handleError);
     }
 
     getHero(id: number): Promise<Hero> {
         return this.getHeroes()
             .then(heroes => heroes.find(hero => hero.id === id));
+    }
+
+    _handleError(error: any): Promise<any> {
+        console.error('An error occurred', error); // for demo purposes only
+        return Promise.reject(error.message || error);
     }
 }
